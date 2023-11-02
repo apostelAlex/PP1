8ee6340e4374401876f540daddd90d73e7fc52ad
Alexandros Apostolidis <ierosa2smix@gmail.com>
2023-11-02 14:40:25 +0100

    A1 added.

--- p7/StringTest.java
+++ p7/StringTest.java
@@ -3,22 +3,36 @@ public class StringTest {
     /* *** Aufgabe 1a *** */    
     // 2
     public static String filterLetters(String s) {
-        
-        return " ";  // Dummy Return
+        StringBuffer res = new StringBuffer();
+        char[] cs = s.toCharArray();
+        for (char c : cs) {
+            if (Character.isLetter(c)){
+                res.append(Character.toLowerCase(c));
+            }
+        }
+        return res.toString();  // Dummy Return
     }
 
 
     /* *** Aufgabe 1b *** */
     // 2.5
     public static boolean isPalindromeIt(String s) {
-        
-        return false; // Dummy Return
+        for (int i = 0; i <= s.length()-i; i++) {
+            if (s.charAt(i) != s.charAt(s.length()-i-1)){
+                return false;
+            }
+        }
+        return true; // Dummy Return
     }
 
     /* *** Aufgabe 1c *** */
     // 2.5
     public static boolean isPalindromeRek(String s) {
-        
+        int l = s.length()-1;
+        if(l<1)
+            return true;
+        if (s.charAt(0) == s.charAt(l))
+            return isPalindromeIt(s.substring(1, l));
         return false;    // Dummy Return
     }
     
